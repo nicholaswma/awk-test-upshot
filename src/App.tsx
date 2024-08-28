429d76ab298d54885b947cf43237476c21f0621a
7i7o <mparij@communitylabs.com>
2024-08-28 18:12:01 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { ConnectButton } from "arweave-wallet-kit";
+
+function App() {
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <ConnectButton
+        showBalance={false}
+        showProfilePicture={false}
+        useAns={false}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ArweaveWalletKit
+      config={{
+        permissions: [
+          "ACCESS_ADDRESS",
+          "ACCESS_ALL_ADDRESSES",
+          "ACCESS_PUBLIC_KEY",
+          "DISPATCH",
+          "SIGN_TRANSACTION",
+        ],
+        ensurePermissions: true,
+      }}
+    >
+      <App />
+    </ArweaveWalletKit>{" "}
+  </StrictMode>
+);
