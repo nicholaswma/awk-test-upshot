ca09239ce51f2b4b27310d1b27e104d9caaf8194
7i7o <mparij@communitylabs.com>
2024-09-26 00:56:35 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ function App() {
   const { connected } = useConnection();
   return (
     <div className="flex flex-col py-8 space-y-4 w-full h-screen items-center">
-      <ConnectButton
-        showBalance={false}
-        showProfilePicture={false}
-        useAns={false}
-        className="self-end mr-8"
-      />
+      <div className="flex w-full px-8 items-center place-content-between">
+        <div className="text-3xl font-bold drop-shadow-md shadow-slate-900">
+          Arweave Wallet Kit Tester
+        </div>
+        <ConnectButton
+          showBalance={false}
+          showProfilePicture={false}
+          useAns={false}
+          className="self-end mr-8"
+        />
+      </div>
       {connected && <ArweaveActions />}
     </div>
   );
