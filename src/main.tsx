0bf922876ccc05386cd5321350ba549c237954c0
7i7o <mparij@communitylabs.com>
2024-11-08 17:54:55 +0100

    fix build errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
